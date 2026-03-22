76fbe9c0582c2e9ed5a5768f0ea3a6bb55febfda
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-03-22 15:17:00 -0300

    FEAT: Create the ApiResponse model class

---
+++ AgroMarketplace.Api/Models/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AgroMarketplace.Api.Models
+{
+    public class ApiResponse <T> where T : class
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+    }
+}
