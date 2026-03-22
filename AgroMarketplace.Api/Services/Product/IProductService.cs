e129c93633bbddd92199d691dc8ea244e334c9a7
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-03-22 15:18:24 -0300

    FEAT: Create the IProductService interface

---
+++ AgroMarketplace.Api/Services/Product/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using AgroMarketplace.Api.Models;
+using AgroMarketplace.Api.Models.DTOS.Request;
+using AgroMarketplace.Api.Models.DTOS.Response;
+
+namespace AgroMarketplace.Api.Services.Product
+{
+    public interface IProductService
+    {
+        ApiResponse<ProductResponseDto> CreateProductAsync(CreateProductRequestDto request);
+    }
+}
