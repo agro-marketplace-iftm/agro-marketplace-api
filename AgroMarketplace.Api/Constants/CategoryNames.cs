642dee855ce10ab6cd70825542b8c54e86177220
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-03 15:53:10 -0300

    FEAT: Create the CategoryNames constant class

---
+++ AgroMarketplace.Api/Constants/CategoryNames.cs
@@ -0,0 +1,12 @@
+﻿namespace AgroMarketplace.Api.Constants
+{
+    public static class CategoryNames
+    {
+        public const string Grains = "Grains";       // Grãos (Milho, Soja, etc)
+        public const string Produce = "Produce";     // Hortifruti (Frutas, Verduras, Legumes)
+        public const string Dairy = "Dairy";         // Laticínios (Queijos, Leite, Ovos)
+        public const string Artisanal = "Artisanal"; // Artesanais (Mel, Geleias, Conservas)
+        public const string Plants = "Plants";       // Mudas, Cactos, Plantas
+        public const string Others = "Others";       // Outros
+    }
+}
