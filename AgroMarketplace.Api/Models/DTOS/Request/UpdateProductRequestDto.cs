9854c00eccf06b5fd078557800cb96b9b8a1d01d
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-03 14:37:58 -0300

    FEAT: Create the UpdateProduct dto

---
+++ AgroMarketplace.Api/Models/DTOS/Request/UpdateProductRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AgroMarketplace.Api.Models.DTOS.Request;
+
+public class UpdateProductRequestDto
+{
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public decimal Price { get; set; }
+    public string Category { get; set; } = null!;
+    public string ImageUrl { get; set; } = null!;
+    public int Stock { get; set; }
+}
